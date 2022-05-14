0985d87fe51d1f800e97684b08a1d323d55cf90e
Oshan Ivantha <oshan.ivantha@gmail.com>
2022-05-14 12:28:04 +0530

    Fix links

--- src/pages/people.js
+++ src/pages/people.js
@@ -12,25 +12,29 @@ const PeoplePage = () => (
                 <h2>Advisors</h2>
                 <ul className="people-wrapper">
                     <li className="content-text">
-                        Dr. Nalin Ranasinghe <br />
+                        <a className="link" href="https://ucsc.cmb.ac.lk/profile/dnr/" target="_blank" rel="noopener noreferrer">Dr. Nalin Ranasinghe</a>
+                        <br/>
                         <span className="institute-text">
                             Senior Lecturer | University of Colombo
                         </span>
                     </li>
                     <li className="content-text">
-                        Dr. Mohammad Nabeel <br />
+                        <a className="link" href="https://www.hbku.edu.qa/en/staff/dr-mohamed-nabeel" target="_blank" rel="noopener noreferrer">Dr. Mohammad Nabeel</a>
+                        <br/>
                         <span className="institute-text">
                             Research Scientist | Qatar Computing Research Institute (QCRI)
                         </span>
                     </li>
                     <li className="content-text">
-                        Mr. Gihan Seneviratne <br />
+                        <a className="link" href="https://ucsc.cmb.ac.lk/profile/gps/" target="_blank" rel="noopener noreferrer">Mr. Gihan Seneviratne</a>
+                        <br/>
                         <span className="institute-text">
                             Senior Lecturer | University of Colombo
                         </span>
                     </li>
                     <li className="content-text">
-                        Dr. Noel Fernando <br />
+                        <a className="link" href="https://ucsc.cmb.ac.lk/profile/nas/" target="_blank" rel="noopener noreferrer">Dr. Noel Fernando</a>
+                        <br/>
                         <span className="institute-text">
                             Senior Lecturer | University of Colombo
                         </span>
@@ -39,26 +43,29 @@ const PeoplePage = () => (
                 <h2 className="people-subtitle">Colleagues</h2>
                 <ul className="people-wrapper">
                     <li className="content-text">
-                        Ms. Disni Rathnayake <br />
+                        <a className="link" href="https://www.linkedin.com/in/disni-rathnayake-3178b210b/" target="_blank" rel="noopener noreferrer">Ms. Disni Rathnayake</a>
+                        <br/>
                         <span className="institute-text">
-                        Research Assistant | University of Colombo
+                            Research Assistant | University of Colombo
                         </span>
                     </li>
                     <li className="content-text">
-                        Mr. Amila Indika <br />
+                        <a className="link" href="https://www.linkedin.com/in/amila-indika/" target="_blank" rel="noopener noreferrer">Mr. Amila Indika</a>
+                        <br/>
                         <span className="institute-text">
-                        Lecturer (On contract) | University of Peradeniya
+                            Lecturer (On contract) | University of Peradeniya
                         </span>
                     </li>
                     <li className="content-text">
-                        Mr. Indrajith Ekanayake <br />
+                        <a className="link" href="https://www.linkedin.com/in/indrajithek/" target="_blank" rel="noopener noreferrer">Mr. Indrajith Ekanayake</a>
+                        <br/>
                         <span className="institute-text">
-                        Visiting Lecturer | Open University of Sri Lanka
+                            Visiting Lecturer | Open University of Sri Lanka
                         </span>
                     </li>
                 </ul>
             </div>
-         </div>
+        </div>
     </Layout>
 )
 
