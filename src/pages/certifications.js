714eb972fbf616a3500c77c77174c3a54a204f52
Oshan Ivantha <oshan.ivantha@gmail.com>
2022-05-14 09:42:54 +0530

    Add link colors

--- src/pages/certifications.js
+++ src/pages/certifications.js
@@ -14,111 +14,95 @@ const CertificationsPage = () => (
                     <ul>
                         <li className="certification-wrapper">
                             <h3>2021 | Advanced Machine Learning on Google Cloud Specialization</h3>
-                            <span className="institute-text">Coursera | 
-                                <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/GEQ3Y52BD6QM">
-                                    [Certificate]
-                                </a>
+                            <span className="institute-text">
+                                Coursera | <a className="link" href="https://www.coursera.org/account/accomplishments/specialization/certificate/GEQ3Y52BD6QM">[Certificate]</a>
                             </span>
                             <ul className="content-text">
-                                <li>Image Understanding with TensorFlow on GCP | <a href="https://www.coursera.org/account/accomplishments/certificate/57ARXUU2VDM3">[Certificate]</a></li>
-                                <li>Production Machine Learning Systems | <a href="https://www.coursera.org/account/accomplishments/certificate/YJCN8C34V5HH">[Certificate]</a></li>
-                                <li>Sequence Models for Time Series and Natural Language Processing | <a href="https://www.coursera.org/account/accomplishments/certificate/VPLMEZDD72DX">[Certificate]</a></li>
-                                <li>End-to-End Machine Learning with TensorFlow on GCP | <a href="https://www.coursera.org/account/accomplishments/certificate/KVYB45CAUTFF">[Certificate]</a></li>
-                                <li>Recommendation Systems with TensorFlow on GCP | <a href="https://www.coursera.org/account/accomplishments/certificate/33YXEQY5XTB2">[Certificate]</a></li>
+                                <li>Image Understanding with TensorFlow on GCP | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/57ARXUU2VDM3">[Certificate]</a></li>
+                                <li>Production Machine Learning Systems | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/YJCN8C34V5HH">[Certificate]</a></li>
+                                <li>Sequence Models for Time Series and Natural Language Processing | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/VPLMEZDD72DX">[Certificate]</a></li>
+                                <li>End-to-End Machine Learning with TensorFlow on GCP | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/KVYB45CAUTFF">[Certificate]</a></li>
+                                <li>Recommendation Systems with TensorFlow on GCP | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/33YXEQY5XTB2">[Certificate]</a></li>
                             </ul>
                         </li>
                         <li className="certification-wrapper">
                             <h3>2021 | Data Engineering, Big Data, and Machine Learning on GCP Specialization</h3>
-                            <span className="institute-text">Coursera | 
-                                <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/R839DUKFK5F6">
-                                    [Certificate]
-                                </a>
+                            <span className="institute-text">
+                                Coursera | <a className="link" href="https://www.coursera.org/account/accomplishments/specialization/certificate/R839DUKFK5F6">[Certificate]</a>
                             </span>
                             <ul className="content-text">
-                                <li>Modernizing Data Lakes and Data Warehouses with Google Cloud | <a href="https://www.coursera.org/account/accomplishments/certificate/DVUFZ4E43RMR">[Certificate]</a></li>
-                                <li>Building Batch Data Pipelines on GCP | <a href="https://www.coursera.org/account/accomplishments/certificate/HB2AMEDUZZJA">[Certificate]</a></li>
-                                <li>Smart Analytics, Machine Learning, and AI on GCP | <a href="https://www.coursera.org/account/accomplishments/certificate/LZLMGRVBQT9F">[Certificate]</a></li>
-                                <li>Building Resilient Streaming Analytics Systems on Google Cloud | <a href="https://www.coursera.org/account/accomplishments/certificate/4DB2A5BHEFLA">[Certificate]</a></li>
-                                <li>Google Cloud Big Data and Machine Learning Fundamentals | <a href="https://www.coursera.org/account/accomplishments/certificate/FUMXGLDJVWJE">[Certificate]</a></li>
+                                <li>Modernizing Data Lakes and Data Warehouses with Google Cloud | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/DVUFZ4E43RMR">[Certificate]</a></li>
+                                <li>Building Batch Data Pipelines on GCP | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/HB2AMEDUZZJA">[Certificate]</a></li>
+                                <li>Smart Analytics, Machine Learning, and AI on GCP | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/LZLMGRVBQT9F">[Certificate]</a></li>
+                                <li>Building Resilient Streaming Analytics Systems on Google Cloud | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/4DB2A5BHEFLA">[Certificate]</a></li>
+                                <li>Google Cloud Big Data and Machine Learning Fundamentals | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/FUMXGLDJVWJE">[Certificate]</a></li>
                             </ul>
                         </li>
                         <li className="certification-wrapper">
                             <h3>2021 | Machine Learning on Google Cloud Specialization</h3>
-                            <span className="institute-text">Coursera | 
-                                <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/B4DZM89QCHTY">
-                                    [Certificate]
-                                </a>
+                            <span className="institute-text">
+                                Coursera | <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/B4DZM89QCHTY">[Certificate]</a>
                             </span>
                             <ul className="content-text">
-                                <li>Feature Engineering | <a href="https://www.coursera.org/account/accomplishments/certificate/S6WFSRCBSZEY">[Certificate]</a></li>
-                                <li>Machine Learning in the Enterprise | <a href="https://www.coursera.org/account/accomplishments/certificate/RF3WYWVVHPWN">[Certificate]</a></li>
-                                <li>Launching into Machine Learning | <a href="https://www.coursera.org/account/accomplishments/certificate/38BKHV9VXBJR">[Certificate]</a></li>
-                                <li>How Google does Machine Learning | <a href="https://www.coursera.org/account/accomplishments/certificate/3FPL3H9PNVM9">[Certificate]</a></li>
-                                <li>TensorFlow on Google Cloud | <a href="https://www.coursera.org/account/accomplishments/certificate/UU3NCVUKNJMU">[Certificate]</a></li>
+                                <li>Feature Engineering | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/S6WFSRCBSZEY">[Certificate]</a></li>
+                                <li>Machine Learning in the Enterprise | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/RF3WYWVVHPWN">[Certificate]</a></li>
+                                <li>Launching into Machine Learning | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/38BKHV9VXBJR">[Certificate]</a></li>
+                                <li>How Google does Machine Learning | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/3FPL3H9PNVM9">[Certificate]</a></li>
+                                <li>TensorFlow on Google Cloud | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/UU3NCVUKNJMU">[Certificate]</a></li>
                             </ul>
                         </li>
                         <li className="certification-wrapper">
                             <h3>2020 | Deep Learning Specialization</h3>
-                            <span className="institute-text">Coursera |
-                                <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/UTW8ESZG3W85">
-                                    [Certificate]
-                                </a>
+                            <span className="institute-text">
+                                Coursera | <a className="link" href="https://www.coursera.org/account/accomplishments/specialization/certificate/UTW8ESZG3W85">[Certificate]</a>
                             </span>
                             <ul className="content-text">
-                                <li>Structuring Machine Learning Projects | <a href="https://www.coursera.org/account/accomplishments/certificate/X6PMQBXHUARK">[Certificate]</a></li>
-                                <li>Sequence Models | <a href="https://www.coursera.org/account/accomplishments/certificate/Q8SJ9KQPZAHB">[Certificate]</a></li>
-                                <li>Convolutional Neural Networks | <a href="https://www.coursera.org/account/accomplishments/certificate/YSM4W94E68ER">[Certificate]</a></li>
-                                <li>Improving Deep Neural Networks: Hyperparameter Tuning, Regularization and Optimization | <a href="https://www.coursera.org/account/accomplishments/certificate/MCYT76NMX63A">[Certificate]</a></li>
-                                <li>Neural Networks and Deep Learning | <a href="https://www.coursera.org/account/accomplishments/certificate/AB4BMKLGQMW3">[Certificate]</a></li>
+                                <li>Structuring Machine Learning Projects | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/X6PMQBXHUARK">[Certificate]</a></li>
+                                <li>Sequence Models | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/Q8SJ9KQPZAHB">[Certificate]</a></li>
+                                <li>Convolutional Neural Networks | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/YSM4W94E68ER">[Certificate]</a></li>
+                                <li>Improving Deep Neural Networks: Hyperparameter Tuning, Regularization and Optimization | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/MCYT76NMX63A">[Certificate]</a></li>
+                                <li>Neural Networks and Deep Learning | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/AB4BMKLGQMW3">[Certificate]</a></li>
                             </ul>
                         </li>
                         <li className="certification-wrapper">
                             <h3>2020 | Natural Language Processing Specialization</h3>
-                            <span className="institute-text">Coursera | 
-                                <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/M7CXZX366Y4G">
-                                    [Certificate]
-                                </a>
+                            <span className="institute-text">
+                                Coursera | <a className="link" href="https://www.coursera.org/account/accomplishments/specialization/certificate/M7CXZX366Y4G">[Certificate]</a>
                             </span>
                             <ul className="content-text">
-                                <li>Natural Language Processing with Classification and Vector Spaces | <a href="https://www.coursera.org/account/accomplishments/certificate/S5R6BGQN63XP">[Certificate]</a></li>
-                                <li>Natural Language Processing with Probabilistic Models | <a href="https://www.coursera.org/account/accomplishments/certificate/D29M97L96UJ9">[Certificate]</a></li>
-                                <li>Natural Language Processing with Sequence Models | <a href="https://www.coursera.org/account/accomplishments/certificate/ZXL3K76BF89Q">[Certificate]</a></li>
-                                <li>Natural Language Processing with Attention Models | <a href="https://www.coursera.org/account/accomplishments/certificate/4QE8HCF6MWHA">[Certificate]</a></li>
+                                <li>Natural Language Processing with Classification and Vector Spaces | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/S5R6BGQN63XP">[Certificate]</a></li>
+                                <li>Natural Language Processing with Probabilistic Models | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/D29M97L96UJ9">[Certificate]</a></li>
+                                <li>Natural Language Processing with Sequence Models | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/ZXL3K76BF89Q">[Certificate]</a></li>
+                                <li>Natural Language Processing with Attention Models | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/4QE8HCF6MWHA">[Certificate]</a></li>
                             </ul>
                         </li>
                         <li className="certification-wrapper">
                             <h3>2020 | DeepLearning.AI TensorFlow Developer Specialization</h3>
-                            <span className="institute-text">Coursera |
-                                <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/B5J28B9449EC">
-                                    [Certificate]
-                                </a>
+                            <span className="institute-text">
+                                Coursera | <a className="link" href="https://www.coursera.org/account/accomplishments/specialization/certificate/B5J28B9449EC">[Certificate]</a>
                             </span>
                             <ul className="content-text">
-                                <li>Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning | <a href="https://www.coursera.org/account/accomplishments/certificate/9TFETSB3DF65">[Certificate]</a></li>
-                                <li>Convolutional Neural Networks in TensorFlow | <a href="https://www.coursera.org/account/accomplishments/certificate/YM4BE6MRGTVV">[Certificate]</a></li>
-                                <li>Natural Language Processing in TensorFlow | <a href="https://www.coursera.org/account/accomplishments/certificate/PGPJXE42GXST">[Certificate]</a></li>
-                                <li>Sequences, Time Series and Prediction | <a href="https://www.coursera.org/account/accomplishments/certificate/NXHMWPJWENJK">[Certificate]</a></li>
+                                <li>Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/9TFETSB3DF65">[Certificate]</a></li>
+                                <li>Convolutional Neural Networks in TensorFlow | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/YM4BE6MRGTVV">[Certificate]</a></li>
+                                <li>Natural Language Processing in TensorFlow | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/PGPJXE42GXST">[Certificate]</a></li>
+                                <li>Sequences, Time Series and Prediction | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/NXHMWPJWENJK">[Certificate]</a></li>
                             </ul>
                         </li>
                         <li className="certification-wrapper">
                             <h3>2020 | Generative Adversarial Networks (GANs) Specialization</h3>
-                            <span className="institute-text">Coursera |
-                                <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/UH6H72AWTZB4">
-                                    [Certificate]
-                                </a>
+                            <span className="institute-text">
+                                Coursera | <a className="link" href="https://www.coursera.org/account/accomplishments/specialization/certificate/UH6H72AWTZB4">[Certificate]</a>
                             </span>
                             <ul className="content-text">
-                                <li>Build Basic Generative Adversarial Networks (GANs) | <a href="https://www.coursera.org/account/accomplishments/certificate/D24VTC2D6TDJ">[Certificate]</a></li>
-                                <li>Build Better Generative Adversarial Networks (GANs) | <a href="https://www.coursera.org/account/accomplishments/certificate/AAVZ2QRMWZ9Z">[Certificate]</a></li>
-                                <li>Apply Generative Adversarial Networks (GANs) | <a href="https://www.coursera.org/account/accomplishments/certificate/SWKNUEDARL37">[Certificate]</a></li>
+                                <li>Build Basic Generative Adversarial Networks (GANs) | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/D24VTC2D6TDJ">[Certificate]</a></li>
+                                <li>Build Better Generative Adversarial Networks (GANs) | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/AAVZ2QRMWZ9Z">[Certificate]</a></li>
+                                <li>Apply Generative Adversarial Networks (GANs) | <a className="link" href="https://www.coursera.org/account/accomplishments/certificate/SWKNUEDARL37">[Certificate]</a></li>
                             </ul>
                         </li>
                         <li className="certification-wrapper">
                             <h3>2020 | Qwicklabs quests on ML and Data Science</h3>
-                            <span className="institute-text">Qwicklabs |
-                                <a href="https://www.qwiklabs.com/public_profiles/a3117be0-5025-4aea-a423-07b1c7bfc75a">
-                                    [Certificate]
-                                </a>
+                            <span className="institute-text">
+                                Qwicklabs | <a className="link" href="https://www.qwiklabs.com/public_profiles/a3117be0-5025-4aea-a423-07b1c7bfc75a">[Certificate]</a>
                             </span>
                         </li>
                     </ul>
