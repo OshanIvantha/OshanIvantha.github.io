2c768a3464833a150e641d4a23d9ff7cef148337
PamudiAD <pamudi.dahanayake@gmail.com>
2022-05-16 04:13:44 +0530

    home and about page styles added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,40 +1,65 @@
 import React from "react"
-
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 
 const AboutPage = () => (
     <Layout>
-        <div className="section-wrapper">
+        <div className="aboutLayout section-wrapper">
             <div className="section-title">
                 <h1>About</h1>
             </div>
             <div className="section-items">
                 <div>
-                    <h2>Education</h2>
-                    <p className="note">Add logos to education</p>
-                    <div>
-                        <p>University of Colombo School of Computing</p>
-                        <p>B.Sc (Hons) in Computer Science</p>
-                        <p>Feb. 2016 - Feb. 2020</p>
-                        <p>First Class Honors with a GPA of 3.61/4.0 (Top 5%)</p>
-                        <p>Ranked 6th over 156 CS students</p>
-                        <p>Thesis: “Summarization of Large Scale Streaming Graphs” (Grade A)</p>
-                        <ul>
-                            <li>Chair of IEEE Student Branch</li>
-                            <li>Core team member of Mozilla Club</li>
-                            <li>Member of CompSoc</li>
-                            <li>Instructor of Exploration Club</li>
-                            <li>Batch representative at IUD</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p>Maliyadeva College</p>
-                        <p>G.C.E. Advance Level Examination - Physical Science Stream</p>
-                        <p>Jan. 2001 - Dec. 2014</p>
-                        <ul>
-                            <li>President of Robotics Club</li>
-                            <li>Team Lead of Hardware and Networking Group</li>
-                        </ul>
+                    <div className="education-wrapper">
+                        <h2>Education</h2>
+                        <div className="education-card">
+                            <div className="img-container ucsc-mal">
+                                <StaticImage
+                                    src="../images/ucsc.jpg"
+                                    alt="UCSC"
+                                    placeholder="blurred"
+                                    objectFit="contain"
+                                />
+                            </div>
+                            <div className="card-content">
+                                <h3>University of Colombo School of Computing</h3>
+                                <h3>B.Sc (Hons) in Computer Science</h3>
+                                <span className="tags-text">First Class Honors with a GPA of 3.61/4.0 (Top 5%)</span><br />
+                                <span className="tags-text">Ranked 6th over 156 CS students</span><br />
+                                <span className="tags-text">Thesis: “Summarization of Large Scale Streaming Graphs” (Grade A)</span><br />
+                                <span className="content-text">
+                                    <ul>
+                                        <li>Chair of IEEE Student Branch</li>
+                                        <li>Core team member of Mozilla Club</li>
+                                        <li>Member of CompSoc</li>
+                                        <li>Instructor of Exploration Club</li>
+                                        <li>Batch representative at IUD</li>
+                                    </ul>
+                                </span><br />
+                                <span className="date-text">Feb. 2016 - Feb. 2020</span><br />
+                            </div>
+                        </div>
+                        <div className="education-card">
+                            <div className="img-container ucsc-mal">
+                                <StaticImage
+                                    src="../images/maliyadeva.jpg"
+                                    alt="Maliyadeva College"
+                                    placeholder="blurred"
+                                    objectFit="contain"
+                                />
+                            </div>
+                            <div className="card-content">
+                                <h3>Maliyadeva College</h3>
+                                <span className="tags-text">G.C.E. Advance Level Examination - Physical Science Stream</span><br />
+                                <span className="content-text">
+                                    <ul>
+                                        <li>President of Robotics Club</li>
+                                        <li>Team Lead of Hardware and Networking Group</li>
+                                    </ul>
+                                </span><br />
+                                <span className="date-text">Jan. 2001 - Dec. 2014</span><br />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,57 +8,155 @@ const IndexPage = () => {
     return (
         <Layout>
             <div className="homeLayout section-wrapper">
-                <div className="section-title">
-                    <h1>Home</h1>
-                </div>
                 <div className="section-items">
-                    <div>
-                        {/*<StaticImage src="../images/oim.png" alt="oshan" className="profile-img"/>*/}
-                        <StaticImage
-                            src="../images/oim.png"
-                            alt="oshan"
-                            placeholder="blurred"
-                            layout="fixed"
-                            // width={200}
-                            height={200}
-                            className="profile-img"
-                        />
-                    </div>
-                    <div>
-                        <p className="note">Can we have a collection (somewhere in this site) of logos of the places/orgs/companies that I've been involved in?</p>
-                    </div>
-                    <div>
+                    <div className="about-me">
+                        <div className="profile-card">
+                            <div className="img-container">
+                                <StaticImage
+                                    src="../images/oim.png"
+                                    alt="CORS"
+                                    placeholder="blurred"
+                                    className="article-img"
+                                    objectFit="contain"
+                                />
+                            </div>
+                            <div className="profile-description">
+                                <h2>Hello there,</h2>
+                                <span className="content-text">
+                                    I am a Senior Data Science Engineer at WSO2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Nunc mattis enim ut tellus elementum. Tincidunt augue interdum
+                                    velit euismod. Id velit ut tortor pretium viverra.<br />
+                                    Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Ut tristique et egestas quis ipsum suspendisse ultrices.
+                                    Aliquam eleifend mi in nulla. Proin sed libero enim sed faucibus. Iaculis at erat pellentesque adipiscing commodo elit
+                                    at imperdiet dui. Ornare suspendisse sed nisi lacus sed viverra tellus in.</span>
+                            </div>
+                        </div>
+                        <div className="research-interests">
+                            <h2>Research Interests</h2>
+                            <span>Reinforcement Learning | Machine Learning | Artificial Intelligence</span><br />
+                            <span>Game Theory | Computer Games</span>
+                        </div>
+                        {/* <div>
+                            <p className="note">Can we have a collection (somewhere in this site) of logos of the places/orgs/companies that I've been involved in?</p>
+                        </div> */}
                         <h2>Contact Details</h2>
-                        <p>Phone: (+94) 71 908 4020</p>
-                        <p>Email: oshan.ivantha@gmail.com</p>
-                        <p>Google Scholar: https://scholar.google.com/citations?user=2FA0RngAAAAJ&hl=en</p>
-                        <p>ResearchGate: https://www.researchgate.net/profile/Oshan-Mudannayake</p>
-                        <p>arXiv: https://arxiv.org/search/cs?searchtype=author&query=Mudannayake%2C+O</p>
-                        <p>Twitter: https://twitter.com/_ivantha</p>
-                        <p>Linkedin: https://www.linkedin.com/in/ivantha/</p>
-                        <p>GitHub: https://github.com/ivantha</p>
+                        <div className="contact-details">
+                            <div className="contact-section">
+                                <div className="contact-item">
+                                    <StaticImage
+                                        src="../images/phone.png"
+                                        alt="Phone"
+                                        placeholder="blurred"
+                                        height={14}
+                                        layout="fixed"
+                                    />
+                                    <span>(+94) 71 908 4020</span>
+                                </div>
+                                <div className="contact-item">
+                                    <StaticImage
+                                        src="../images/email.png"
+                                        alt="email"
+                                        placeholder="blurred"
+                                        height={11}
+                                        layout="fixed"
+                                    />
+                                    <span>oshan.ivantha@gmail.com</span>
+                                </div>
+                                <div className="contact-item">
+                                    <StaticImage
+                                        src="../images/google-scholar.png"
+                                        alt="google scholar"
+                                        placeholder="blurred"
+                                        height={16}
+                                        layout="fixed"
+                                    />
+                                    <span><a className="link" href="https://scholar.google.com/citations?user=2FA0RngAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">Google Scholar</a></span>
+                                </div>
+                                <div className="contact-item">
+                                    <StaticImage
+                                        src="../images/research-gate.png"
+                                        alt="research gate"
+                                        placeholder="blurred"
+                                        height={16}
+                                        layout="fixed"
+                                    />
+                                    <span><a className="link" href="https://www.researchgate.net/profile/Oshan-Mudannayake" target="_blank" rel="noopener noreferrer">ResearchGate</a></span>
+                                </div>
+                            </div>
+                            <div className="contact-section">
+                                <div className="contact-item">
+                                    <StaticImage
+                                        src="../images/arxiv.png"
+                                        alt="arXiv"
+                                        placeholder="blurred"
+                                        height={16}
+                                        layout="fixed"
+                                    />
+                                    <span><a className="link" href="https://arxiv.org/search/cs?searchtype=author&query=Mudannayake%2C+O" target="_blank" rel="noopener noreferrer">arXiv</a></span>
+                                </div>
+                                <div className="contact-item">
+                                    <StaticImage
+                                        src="../images/twitter.png"
+                                        alt="twitter"
+                                        placeholder="blurred"
+                                        height={13}
+                                        layout="fixed"
+                                    />
+                                    <span><a className="link" href="https://twitter.com/_ivantha" target="_blank" rel="noopener noreferrer">_ivantha</a></span>
+                                </div>
+                                <div className="contact-item">
+                                    <StaticImage
+                                        src="../images/linkedin-in.png"
+                                        alt="linkedin-in"
+                                        placeholder="blurred"
+                                        height={15}
+                                        layout="fixed"
+                                    />
+                                    <span><a className="link" href="https://www.linkedin.com/in/ivantha/" target="_blank" rel="noopener noreferrer">ivantha</a></span>
+                                </div>
+                                <div className="contact-item">
+                                    <StaticImage
+                                        src="../images/github.png"
+                                        alt="github"
+                                        placeholder="blurred"
+                                        height={15}
+                                        layout="fixed"
+                                    />
+                                    <span><a className="link" href="https://github.com/ivantha" target="_blank" rel="noopener noreferrer">ivantha</a></span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <h2>Research Interests</h2>
-                        <p>Reinforcement Learning | Machine Learning | Artificial Intelligence</p>
-                        <p>Game Theory | Computer Games</p>
-                    </div>
-                    <div>
-                        <h2>[About Me]</h2>
-                        <p>I am a Senior Data Science Engineer at WSO2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc mattis enim ut tellus elementum. Tincidunt augue interdum velit euismod. Id velit ut tortor pretium viverra.</p>
-                        <p>Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Ut tristique et egestas quis ipsum suspendisse ultrices. Aliquam eleifend mi in nulla. Proin sed libero enim sed faucibus. Iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui. Ornare suspendisse sed nisi lacus sed viverra tellus in.</p>
-                        <p>Pretium viverra suspendisse potenti nullam ac tortor vitae purus. Mollis aliquam ut porttitor leo a diam. Lorem donec massa sapien faucibus. Fringilla ut morbi tincidunt augue interdum velit euismod. Massa tincidunt nunc pulvinar sapien et ligula. Mauris augue neque gravida in fermentum. Libero volutpat sed cras ornare. Morbi leo urna molestie at elementum eu. Magna eget est lorem ipsum. Sem viverra aliquet eget sit amet tellus.</p>
-                    </div>
-                    <div>
+                    <div className="news-section">
                         <h2>News</h2>
-                        <p>2022.04.17 - Started working on 'Sinhala OCR' with Indrajith Ekanayake.</p>
-                        <p>2022.03.18 - Started working on 'Privacy Preserved Machine Learning' with Dr. Upul Jayasinghe and Amila Indika.</p>
-                        <p>2022.03.18 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..</p>
-                        <p>2022.03.18 -  Ut tristique et egestas quis. Cursus turpis massa tincidunt dui.</p>
-                        <p>2022.03.18 - Sapien faucibus et molestie ac feugiat sed lectus vestibulum.</p>
-                        <p>2022.03.18 - Integer quis auctor elit sed vulputate mi sit. Facilisis volutpat est velit egestas dui id. In hac habitasse platea dictumst vestibulum rhoncus.</p>
-                        <p>2022.03.18 - At imperdiet dui accumsan sit amet.</p>
-                        <p>2022.03.18 - Viverra suspendisse potenti nullam ac. Augue ut lectus arcu bibendum at varius.</p>
+                        <div className="news-content">
+                            <span className="date-text">April 17, 2022</span><br />
+                            <span className="content-text">Started working on 'Sinhala OCR' with Indrajith Ekanayake.</span>
+                        </div>
+                        <div className="news-content">
+                            <span className="date-text">March 18, 2022</span><br />
+                            <span className="content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                        </div>
+                        <div className="news-content">
+                            <span className="date-text">March 18, 2022</span><br />
+                            <span className="content-text">Ut tristique et egestas quis. Cursus turpis massa tincidunt dui.</span>
+                        </div>
+                        <div className="news-content">
+                            <span className="date-text">March 18, 2022</span><br />
+                            <span className="content-text">Sapien faucibus et molestie ac feugiat sed lectus vestibulum.</span>
+                        </div>
+                        <div className="news-content">
+                            <span className="date-text">March 18, 2022</span><br />
+                            <span className="content-text">Integer quis auctor elit sed vulputate mi sit. Facilisis volutpat est velit egestas dui id. In hac habitasse platea dictumst vestibulum rhoncus.</span>
+                        </div>
+                        <div className="news-content">
+                            <span className="date-text">March 18, 2022</span><br />
+                            <span className="content-text">At imperdiet dui accumsan sit amet.</span>
+                        </div>
+                        <div className="news-content">
+                            <span className="date-text">March 18, 2022</span><br />
+                            <span className="content-text">Viverra suspendisse potenti nullam ac. Augue ut lectus arcu bibendum at varius.</span>
+                        </div>
                     </div>
                 </div>
             </div>
