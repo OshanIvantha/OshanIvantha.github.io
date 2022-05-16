0cbcccb3d0aa3a190f7c88e1db8048beac44fe8b
Oshan Ivantha <oshan.ivantha@gmail.com>
2022-05-16 06:48:44 +0530

    Use template for Achievements

--- src/pages/achievements.js
+++ src/pages/achievements.js
@@ -2,6 +2,99 @@ import React from "react"
 
 import Layout from "../components/layout"
 
+const achievements = [
+    {
+        place: "Merit Award",
+        event: "Datathon (SLIIT Codefest) by Sri Lanka Institute of Information Technology",
+        year: "2019",
+        country: "Sri Lanka"
+    },
+    {
+        place: "1st place",
+        event: "Codezilla by Mozilla Campus Club of Uva Wellassa University",
+        year: "2018",
+        country: "Sri Lanka"
+    },
+    {
+        place: "1st place",
+        event: "LetMeHack by Sabaragamuwa University",
+        year: "2018",
+        country: "Sri Lanka"
+    },
+    {
+        place: "Participant",
+        event: "HackZurich by Digital Festival",
+        year: "2019",
+        country: "Switzerland"
+    },
+    {
+        place: "Participant",
+        event: "Hack The North by University of Waterloo",
+        year: "2018",
+        country: "Canada"
+    },
+    {
+        place: "2nd place",
+        event: "Presistent Hackathon by Persistent Systems Lanka",
+        year: "2017",
+        country: "Sri Lanka"
+    },
+    {
+        place: "2nd place",
+        event: "HACKiNTRA by University of Jaffna",
+        year: "2017",
+        country: "Sri Lanka"
+    },
+    {
+        place: "Finalist",
+        event: "CodeFest Designathon by Sri Lanka Institute of Information Technology",
+        year: "2017",
+        country: "Sri Lanka"
+    },
+    {
+        place: "Finalist",
+        event: "SS12 Asia by IEEE",
+        year: "2017",
+        country: "Sri Lanka"
+    },
+    {
+        place: "3rd place",
+        event: "Dialog Game Dev Hackathon by Dialog Axiata",
+        year: "2017",
+        country: "Sri Lanka"
+    },
+    {
+        place: "1st place",
+        event: "Mozilla Web Maker Party by Mozilla Club of University of Colombo School of Computing",
+        year: "2016",
+        country: "Sri Lanka"
+    },
+    {
+        place: "2nd place",
+        event: "CodeFest Tertiary Hackathon by Sri Lanka Institute of Information Technology",
+        year: "2016",
+        country: "Sri Lanka"
+    },
+    {
+        place: "1st place",
+        event: "IEEE preXtreme 1.0 by IEEE Student Branch of University of Colombo School of Computing",
+        year: "2016",
+        country: "Sri Lanka"
+    },
+    {
+        place: "6th place",
+        event: "ACES Coders by University of Peradeniya",
+        year: "2016",
+        country: "Sri Lanka"
+    },
+    {
+        place: "5th place",
+        event: "Code Expertz by University of Moratuwa",
+        year: "2016",
+        country: "Sri Lanka"
+    },
+]
+
 const AchievementsPage = () => (
     <Layout>
         <div className="achievementsLayout section-wrapper">
@@ -10,81 +103,13 @@ const AchievementsPage = () => (
             </div>
             <div className="section-items">
                 <ul>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2019 | Merit Award</span>
-                        <h3>Datathon (SLIIT Codefest) by Sri Lanka Institute of Information Technology</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2018 | 1st place</span>
-                        <h3>Codezilla by Mozilla Campus Club of Uva Wellassa University</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2018 | 1st place</span>
-                        <h3>LetMeHack by Sabaragamuwa University</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2019 | Participant</span>
-                        <h3>HackZurich by Digital Festival</h3>
-                        <span className="institute-text country">Switzerland</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2018 | Participant</span>
-                        <h3>Hack The North by University of Waterloo</h3>
-                        <span className="institute-text country">Canada</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2017 | 2nd place</span>
-                        <h3>Presistent Hackathon by Persistent Systems Lanka</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2017 | 2nd place</span>
-                        <h3>HACKiNTRA by University of Jaffna</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2017 | Finalist</span>
-                        <h3>CodeFest Designathon by Sri Lanka Institute of Information Technology</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2017 | Finalist</span>
-                        <h3>SS12 Asia by IEEE</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2017 | 3rd place</span>
-                        <h3>Dialog Game Dev Hackathon by Dialog Axiata</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2016 | 1st place</span>
-                        <h3>Mozilla Web Maker Party by Mozilla Club of University of Colombo School of Computing</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2016 | 2nd place</span>
-                        <h3>CodeFest Tertiary Hackathon by Sri Lanka Institute of Information Technology</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2016 | 1st place</span>
-                        <h3>IEEE preXtreme 1.0 by IEEE Student Branch of University of Colombo School of Computing</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2016 | 6th place</span>
-                        <h3>ACES Coders by University of Peradeniya</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
-                    <li className="achievements-wrapper">
-                        <span className="institute-text">2016 | 5th place</span>
-                        <h3>Code Expertz by University of Moratuwa</h3>
-                        <span className="institute-text country">Sri Lanka</span>
-                    </li>
+                    {achievements.map(achievement => (
+                        <li key={achievement.url} className="achievements-wrapper">
+                            <span className="institute-text">{achievement.year} | {achievement.place}</span>
+                            <h3>{achievement.event}</h3>
+                            <span className="institute-text country">{achievement.country}</span>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
