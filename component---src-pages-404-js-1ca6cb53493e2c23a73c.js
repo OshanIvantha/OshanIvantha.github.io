db4c3347cb5fc3ab22c879816f4a3f38f5eb78e9
Oshan Ivantha <oshan.ivantha@gmail.com>
2022-05-16 09:27:04 +0530

    Updates

---
+++ component---src-pages-404-js-1ca6cb53493e2c23a73c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkivantha=self.webpackChunkivantha||[]).push([[883],{429:function(e,n,t){t.r(n);var a=t(7294);n.default=function(){return a.createElement("main",null,a.createElement("title",null,"Not found"),a.createElement("h1",null,"Page not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1ca6cb53493e2c23a73c.js.map
