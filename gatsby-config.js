0d70ab02b87e66ae15d28b026e02451ee9aac179
Oshan Ivantha <oshan.ivantha@gmail.com>
2022-05-16 09:45:30 +0530

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,22 +13,36 @@ module.exports = {
         ],
         siteUrl: `https://ivantha.com`,
     },
-    plugins: ["gatsby-plugin-sass", {
-        resolve: "gatsby-plugin-google-analytics",
-        options: {
-            "trackingId": "G-9BMKFHVG74",
+    plugins: [
+        "gatsby-plugin-sass",
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                "trackingId": "G-9BMKFHVG74",
+            },
         },
-    }, "gatsby-plugin-image", {
-        resolve: "gatsby-plugin-manifest",
-        options: {
-            "icon": "src/images/icon.png",
+        "gatsby-plugin-image",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                "icon": "src/images/icon.png",
+            },
         },
-    }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-        resolve: "gatsby-source-filesystem",
-        options: {
-            "name": "images",
-            "path": "./src/images/",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                "name": "images",
+                "path": "./src/images/",
+            },
+            __key: "images",
         },
-        __key: "images",
-    }],
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/favicon.png',
+            },
+        },
+    ],
 }
