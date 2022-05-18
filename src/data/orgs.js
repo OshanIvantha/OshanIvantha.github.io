5a6cc31b2205b8622d2c9bb5f22b63841feb0602
Oshan Ivantha <oshan.ivantha@gmail.com>
2022-05-18 16:57:12 +0530

    Add org full anchors

--- src/data/orgs.js
+++ src/data/orgs.js
@@ -2,51 +2,54 @@ import React from "react"
 
 export default {
     dreamteam: {
-        fullname: "Dreamteam IT Solutions Pvt Ltd",
+        fullname: "Dreamteam IT Solutions",
         shortname: "Dreamteam IT Solutions",
-        url: "ssssss",
-        anchor: <a className="link" href="sssss" target="_blank" rel="noopener noreferrer">Dreamteam IT Solutions</a>
+        url: "#",
+        anchor: <a className="link" href="#" target="_blank" rel="noopener noreferrer">Dreamteam IT Solutions</a>,
     },
     ucsc: {
         fullname: "Univesity of Colombo School of Computing",
         shortname: "UCSC",
         url: "https://ucsc.cmb.ac.lk/",
-        anchor: <a className="link" href="https://ucsc.cmb.ac.lk/" target="_blank" rel="noopener noreferrer">UCSC</a>
+        anchor: <a className="link" href="https://ucsc.cmb.ac.lk/" target="_blank" rel="noopener noreferrer">UCSC</a>,
+        anchorfullname: <a className="link" href="https://ucsc.cmb.ac.lk/" target="_blank" rel="noopener noreferrer">Univesity of Colombo School of Computing</a>,
     },
     cernhsf: {
         fullname: "High-Energy Physics Software Foundation",
         shortname: "CERN-HSF",
         url: "https://hepsoftwarefoundation.org/",
-        anchor: <a className="link" href="https://hepsoftwarefoundation.org/" target="_blank" rel="noopener noreferrer">CERN-HSF</a>
+        anchor: <a className="link" href="https://hepsoftwarefoundation.org/" target="_blank" rel="noopener noreferrer">CERN-HSF</a>,
     },
     owncloud: {
         fullname: "ownCloud",
         shortname: "ownCloud",
         url: "https://owncloud.com/",
-        anchor: <a className="link" href="https://owncloud.com/" target="_blank" rel="noopener noreferrer">ownCloud</a>
+        anchor: <a className="link" href="https://owncloud.com/" target="_blank" rel="noopener noreferrer">ownCloud</a>,
     },
     aarnet: {
         fullname: "Australian Academic and Research Network",
         shortname: "AARNet",
         url: "https://www.aarnet.edu.au/",
-        anchor: <a className="link" href="https://www.aarnet.edu.au/" target="_blank" rel="noopener noreferrer">AARNet</a>
+        anchor: <a className="link" href="https://www.aarnet.edu.au/" target="_blank" rel="noopener noreferrer">AARNet</a>,
     },
     score: {
-        fullname: "Sustainable COmputing Research Lab",
+        fullname: "Sustainable Computing Research Lab",
         shortname: "SCoRe Lab",
         url: "https://scorelab.org/",
-        anchor: <a className="link" href="https://scorelab.org/" target="_blank" rel="noopener noreferrer">SCoRe Lab</a>
+        anchor: <a className="link" href="https://scorelab.org/" target="_blank" rel="noopener noreferrer">SCoRe Lab</a>,
+        anchorfullname: <a className="link" href="https://scorelab.org/" target="_blank" rel="noopener noreferrer">Sustainable Computing Research Lab</a>,
     },
     qcri: {
         fullname: "Qatar Computing Research Institute",
         shortname: "QCRI",
         url: "https://www.hbku.edu.qa/en/qcri/about",
-        anchor: <a className="link" href="https://www.hbku.edu.qa/en/qcri/about" target="_blank" rel="noopener noreferrer">QCRI</a>
+        anchor: <a className="link" href="https://www.hbku.edu.qa/en/qcri/about" target="_blank" rel="noopener noreferrer">QCRI</a>,
+        anchorfullname: <a className="link" href="https://www.hbku.edu.qa/en/qcri/about" target="_blank" rel="noopener noreferrer">Qatar Computing Research Institute</a>,
     },
     wso2: {
         fullname: "WSO2",
         shortname: "WSO2",
         url: "https://wso2.com/",
-        anchor: <a className="link" href="https://wso2.com/" target="_blank" rel="noopener noreferrer">WSO2</a>
+        anchor: <a className="link" href="https://wso2.com/" target="_blank" rel="noopener noreferrer">WSO2</a>,
     },
 }
