a05f48d560ceb0ee2d48f0ba1ad92b04b6f7cda3
Oshan Ivantha <oshan.ivantha@gmail.com>
2022-07-26 23:26:44 +0530

    Fix People

--- src/pages/people.js
+++ src/pages/people.js
@@ -4,62 +4,17 @@ import Layout from "../components/layout"
 import people from "../data/people"
 
 const alphabeticalList = [
-    {
-        name: people.amilaindika.namewithhonorifics,
-        position: people.amilaindika.position,
-        web: people.amilaindika.web,
-        linkedin: people.amilaindika.linkedin,
-        scholar: people.amilaindika.scholar,
-        researchgate: people.amilaindika.scholar,
-    },
-    {
-        name: people.disnirathnayake.namewithhonorifics,
-        position: people.disnirathnayake.position,
-        web: people.disnirathnayake.web,
-        linkedin: people.disnirathnayake.linkedin,
-        scholar: people.disnirathnayake.scholar,
-        researchgate: people.disnirathnayake.scholar,
-    },
-    {
-        name: people.indrajithekanayake.namewithhonorifics,
-        position: people.indrajithekanayake.position,
-        web: people.indrajithekanayake.web,
-        linkedin: people.indrajithekanayake.linkedin,
-        scholar: people.indrajithekanayake.scholar,
-        researchgate: people.indrajithekanayake.scholar,
-    },
-    {
-        name: people.gihanseneviratne.namewithhonorifics,
-        position: people.gihanseneviratne.position,
-        web: people.gihanseneviratne.web,
-        linkedin: people.gihanseneviratne.linkedin,
-        scholar: people.gihanseneviratne.scholar,
-        researchgate: people.gihanseneviratne.scholar,
-    },
-    {
-        name: people.mohammadnabeel.namewithhonorifics,
-        position: people.mohammadnabeel.position,
-        web: people.mohammadnabeel.web,
-        linkedin: people.mohammadnabeel.linkedin,
-        scholar: people.mohammadnabeel.scholar,
-        researchgate: people.mohammadnabeel.scholar,
-    },
-    {
-        name: people.nalinranasinghe.namewithhonorifics,
-        position: people.nalinranasinghe.position,
-        web: people.nalinranasinghe.web,
-        linkedin: people.nalinranasinghe.linkedin,
-        scholar: people.nalinranasinghe.scholar,
-        researchgate: people.nalinranasinghe.scholar,
-    },
-    {
-        name: people.noelfernando.namewithhonorifics,
-        position: people.noelfernando.position,
-        web: people.noelfernando.web,
-        linkedin: people.noelfernando.linkedin,
-        scholar: people.noelfernando.scholar,
-        researchgate: people.noelfernando.scholar,
-    },
+    people.amilaindika,
+    people.dinalherath,
+    people.dinunifernando,
+    people.disnirathnayake,
+    people.gihanseneviratne,
+    people.harindasamarasekara,
+    people.mohammadnabeel,
+    people.nalinranasinghe,
+    people.noelfernando,
+    people.sumedhedissanayake,
+    people.upuljayasinghe,
 ]
 
 const PeoplePage = () => (
@@ -68,28 +23,42 @@ const PeoplePage = () => (
             <div className="section-title">
                 <h1>People</h1>
             </div>
+            <h2 className="people-subtitle">Present Affiliations</h2>
             <div className="section-items">
-                <ul className="people-wrapper">
+                <ul className="alt-people-wrapper">
                     <li className="content-text">
-                        <span className="institute-text">I am currently supervised by <a className="link" href={people.noelfernando.link} target="_blank" rel="noopener noreferrer">{people.noelfernando.namewithhonorifics}</a> and <a className="link" href={people.mohammadnabeel.link} target="_blank" rel="noopener noreferrer">{people.mohammadnabeel.namewithhonorifics}</a> on multiple research work.</span>
+                        <span className="institute-text">I am collaborating with <a className="link" href={people.amilaindika.link} target="_blank" rel="noopener noreferrer">{people.amilaindika.name}</a> and <a className="link" href={people.upuljayasinghe.link} target="_blank" rel="noopener noreferrer">{people.upuljayasinghe.namewithhonorifics}</a> on "Privacy Preserved Machine Learning using Secure Multi-party Computing".</span>
                     </li>
+                    <li className="content-text">
+                        <span className="institute-text">I am collaborating with <a className="link" href={people.harindasamarasekara.link} target="_blank" rel="noopener noreferrer">{people.harindasamarasekara.name}</a> and <a className="link" href={people.sumedhedissanayake.link} target="_blank" rel="noopener noreferrer">{people.sumedhedissanayake.name}</a> on improving the current state of the Sinhala OCR.</span>
+                    </li>
+                    <li className="content-text">
+                        <span className="institute-text">I am instructed by <a className="link" href={people.dinalherath.link} target="_blank" rel="noopener noreferrer">{people.dinalherath.namewithhonorifics}</a>, <a className="link" href={people.dinunifernando.link} target="_blank" rel="noopener noreferrer">{people.dinunifernando.namewithhonorifics}</a>, and <a className="link" href={people.noelfernando.link} target="_blank" rel="noopener noreferrer">{people.noelfernando.namewithhonorifics}</a> on "Modeling and Prediction of Municipal Solid Waste Generation in Sri Lanka using Machine Learning Techniques".</span>
+                    </li>
+                    <li className="content-text">
+                        <span className="institute-text">I am supervised by <a className="link" href={people.mohammadnabeel.link} target="_blank" rel="noopener noreferrer">{people.mohammadnabeel.namewithhonorifics}</a> on "Machine Learning based Internet Domain Entity Matching and its Applications".</span>
+                    </li>
+                </ul>
+                <div className="people-divider"></div>
+                <h2 className="people-subtitle">Past Affiliations</h2>
+                <ul className="alt-people-wrapper">
                     <li className="content-text">
                         <span className="institute-text">I was advised by <a className="link" href={people.nalinranasinghe.link} target="_blank" rel="noopener noreferrer">{people.nalinranasinghe.namewithhonorifics}</a> in my bachelor's thesis at UCSC.</span>
                     </li>
                 </ul>
                 <div className="people-divider"></div>
-                <h2 className="people-subtitle">Alphabetical list</h2>
+                <h2 className="people-subtitle">Alphabetical List</h2>
                 <ul className="people-wrapper">
                     {alphabeticalList.map(person => (
-                        <li key={person.name} className="content-text">
-                            {person.name}<br/>
-                            <span className="institute-text">{person.position}</span><br/>
-                            <span className="institute-text">
-                                {person.web && <a className="link" href={person.web} target="_blank" rel="noopener noreferrer">[Web] </a>}
-                                {person.linkedin && <a className="link" href={person.linkedin} target="_blank" rel="noopener noreferrer">[Linkedin] </a>}
-                                {person.scholar && <a className="link" href={person.scholar} target="_blank" rel="noopener noreferrer">[Google Scholar]</a>}
-                                {person.researchgate && <a className="link" href={person.researchgate} target="_blank" rel="noopener noreferrer">[ResearchGate]</a>}
-                            </span>
+                        <li key={person.namewithhonorifics} className="content-text">
+                            {person.namewithhonorifics}<br/>
+                                <span className="institute-text">{person.position}</span><br/>
+                                <span className="institute-text">
+                            {person.web && <a className="link" href={person.web} target="_blank" rel="noopener noreferrer">[Web] </a>}
+                            {person.linkedin && <a className="link" href={person.linkedin} target="_blank" rel="noopener noreferrer">[Linkedin] </a>}
+                            {person.scholar && <a className="link" href={person.scholar} target="_blank" rel="noopener noreferrer">[Google Scholar]</a>}
+                            {person.researchgate && <a className="link" href={person.researchgate} target="_blank" rel="noopener noreferrer">[ResearchGate]</a>}
+                                </span>
                         </li>
                     ))}
                 </ul>
