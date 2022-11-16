7fee6cf55f8ad0e115b34ed18879846f5a8aef64
Oshan Mudannayake <oshan.ivantha@gmail.com>
2022-11-16 18:39:19 +0530

    Update SWM publication

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -14,34 +14,31 @@ const PublicationsPage = () => {
                     <h2 className="publications-subtitle">Published Articles</h2>
                     <ul>
                         <li>
+                            <span>[Journal] <b>Oshan Mudannayake</b>, Disni Rathnayake, Jerome Dinal Herath, Dinuni K Fernando, MGNAS Fernando "Exploring Machine Learning and Deep Learning Approaches for Multi-Step Forecasting in Municipal Solid Waste Generation", in <OutboundLink
+                                className="link" href="https://ieeexplore.ieee.org/xpl/RecentIssue.jsp?punumber=6287639" target="_blank" rel="noopener noreferrer">IEEE Access</OutboundLink>. <OutboundLink
+                                className="link" href="https://ieeexplore.ieee.org/document/9950270">DOI: 10.1109/ACCESS.2022.3221941</OutboundLink></span>
+                            <br/>
+                            <span>
+                                <OutboundLink className="link" href="https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=9950270" target="_blank" rel="noopener noreferrer">[PDF]</OutboundLink>
+                                {/*<a className="link" href="xxxxx">[Cite]</a>*/}
+                            </span>
+                        </li>
+                        <li>
                             <span>[Conference] <b>Oshan Mudannayake</b>, Nalin Ranasinghe, "kMatrix: A Space Efficient Streaming Graph Summarization Technique", in <OutboundLink
                                 className="link" href="https://spsr.sltc.ac.lk/events/iciafs-2021/" target="_blank" rel="noopener noreferrer">10th IEEE International Conference on Information and Automation for Sustainability (ICIAfS) 2021</OutboundLink> Colombo, Sri Lanka, Aug. 2021. <OutboundLink
                                 className="link" href="https://ieeexplore.ieee.org/document/9606137">DOI: 10.1109/ICIAfS52090.2021.9606137</OutboundLink></span>
                             <br/>
                             <span>
-                                <OutboundLink className="link" href="/mudannayake2021kmatrix.pdf" target="_blank"
-                                              rel="noopener noreferrer">[PDF]</OutboundLink>
+                                <OutboundLink className="link" href="/mudannayake2021kmatrix.pdf" target="_blank" rel="noopener noreferrer">[PDF]</OutboundLink>
                                 {/*<a className="link" href="xxxxx">[Cite]</a>*/}
                             </span>
                         </li>
                     </ul>
-                    <div className="publications-divider"></div>
-                    <h2 className="publications-subtitle">Accepted Articles</h2>
-                    <ul>
-                        <li>
-                            <span>[Journal] <b>Oshan Mudannayake</b>, Disni Rathnayake, Jerome Dinal Herath, Dinuni K Fernando, MGNAS Fernando "Exploring Machine Learning and Deep Learning Approaches for Multi-Step Forecasting in Municipal Solid Waste Generation"</span>
-                            <br/>
-                            <span>Accepted by <i><OutboundLink className="link" href="https://ieeeaccess.ieee.org/"
-                                                               target="_blank"
-                                                               rel="noopener noreferrer">IEEE Access</OutboundLink></i>.</span>
-                            <br/>
-                            <span>
-                                <OutboundLink className="link" href="/mudannayake2022exploring.pdf" target="_blank"
-                                              rel="noopener noreferrer">[PDF]</OutboundLink>
-                                {/*<a className="link" href="xxxxx">[Cite]</a>*/}
-                            </span>
-                        </li>
-                    </ul>
+                    {/*<div className="publications-divider"></div>*/}
+                    {/*<h2 className="publications-subtitle">Accepted Articles</h2>*/}
+                    {/*<ul>*/}
+                    {/*    */}
+                    {/*</ul>*/}
                     <div className="publications-divider"></div>
                     <h2 className="publications-subtitle">Articles in Review</h2>
                     <ul>
